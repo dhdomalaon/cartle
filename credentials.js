95e7319cd4732d452f3bc07c32dd725b73d61624
dhdomalaon <52355506+dhdomalaon@users.noreply.github.com>
2020-03-22 18:32:50 +0800

    2.0 update

---
+++ credentials.js
@@ -0,0 +1,8 @@
+const user = document.getElementByID('user')
+const password = document.getElementByID('password')
+const form = document.getElementByID('name')
+
+if ((form) === [dean1125])
+{
+    open("www.cartle.online/index.html")
+}
