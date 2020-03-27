4da2d536025713ecead0c97b577ac1c898a91c49
dhdomalaon <52355506+dhdomalaon@users.noreply.github.com>
2020-03-27 11:31:16 +0800

    cred

--- credentials.js
+++ credentials.js
@@ -2,7 +2,9 @@ const user = document.getElementByID('user')
 const password = document.getElementByID('password')
 const form = document.getElementByID('name')
 
-if ((form) === [dean1125])
+
+if (user === dean1125); (password === test)
 {
     open("www.cartle.online/index.html")
 }
+open(www.cartle.com/login)
